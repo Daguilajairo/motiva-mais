b6e87601a6979e933c62ee5ed79f766db8a6bb45
Jairo D'aguila <daguilajairo@gmail.com>
2025-11-25 14:00:33 -0300

    atualizado

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
-// Importando componentes
-import App from "./components/App.jsx";
-import Cadastro from "./components/Cadastro.jsx";
-import Feed from "./components/Feed.jsx";
-import Publicar from "./components/Publicar.jsx";
+import App from './components/App.jsx'
+import Feed from './components/Feed.jsx'
+import Publicar from './components/Publicar.jsx'
+import Cadastro from './components/Cadastro.jsx'
 
-// Root
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="/publicar" element={<Publicar />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
-);
+)
